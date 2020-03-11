c5fa0ef559184c3b5f0f0c17b2dd4170f2ecdf2b
xavi <xavier.flores@enti.cat>
2020-03-11 16:33:48 +0100

    Creació del policia amb porra, afegit sistema de mal i atac tant al jugador com enemic

---
+++ Assets/scripts/PoliciaEstat.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliciaEstat: MonoBehaviour
+{
+	public GameObject player;
+    public Camera cam2d;
+    public GameObject enemyIdle;
+    public GameObject enemyRun;
+    public GameObject enemyAttack;
+    public GameObject enemyTakingDamage;
+    public GameObject enemyDeath;
+    public float timerAttack;
+    public int vida;
+    public float speed;
+	
+    // Update is called once per frame
+    void Update()
+    {
+        //Follow character
+        Vector3 rotVectorEnemy = cam2d.WorldToScreenPoint(this.transform.position);
+        Vector3 rotVectorEnemy2 = cam2d.WorldToScreenPoint(player.transform.position);
+
+        if(vida > 0)
+        {
+            if (rotVectorEnemy.x - rotVectorEnemy2.x > 0)
+                this.transform.rotation = new Quaternion(0, 0, 0, 0);
+            if (rotVectorEnemy.x - rotVectorEnemy2.x <= 0)
+                this.transform.rotation = new Quaternion(0, 180, 0, 0);
+            if (Vector3.Distance(this.transform.position, player.transform.position) > 1 && Vector3.Distance(this.transform.position, player.transform.position) < 8)
+            {
+                Vector3 move = (player.transform.position);
+                this.transform.position += ((move - transform.position).normalized * Time.deltaTime * speed);
+                enemyAttack.SetActive(false);
+                enemyIdle.SetActive(false);
+                enemyRun.SetActive(true);
+            }
+            else if (Vector3.Distance(this.transform.position, player.transform.position) >= 8)
+            {
+                enemyRun.SetActive(false);
+                enemyIdle.SetActive(true);
+            }
+            else
+            {
+                timerAttack -= Time.deltaTime;
+                if(timerAttack <= 0)
+                {
+                    player.GetComponent<playerController>().damage = true;
+                    timerAttack = 2;
+                }
+                if (!Input.GetKey(KeyCode.Mouse0) && player.GetComponent<playerController>().damage == false)
+                {
+                    enemyTakingDamage.SetActive(false);
+                    enemyRun.SetActive(false);
+                    enemyAttack.SetActive(true);
+                }
+                else if(player.GetComponent<playerController>().damage == false)
+                {
+                    enemyRun.SetActive(false);
+                    enemyAttack.SetActive(false);
+                    enemyTakingDamage.SetActive(true);
+                    vida = 0;
+                }
+            }
+        }
+        else
+        {
+            enemyIdle.SetActive(false);
+            enemyTakingDamage.SetActive(false);
+            enemyRun.SetActive(false);
+            enemyAttack.SetActive(false);
+            enemyDeath.SetActive(true);
+        }
+    }
+}
--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public float speed = 2;
+    public float timerdamage;
+    public int life;
+    public Camera Cam2d;
+	public bool hitting;
+    public bool damage;
+	public GameObject playerIdle;
+	public GameObject playerMove;
+	public GameObject playerHitNormal;
+    public GameObject playerReceivesDamage;
 
     // Start is called before the first frame update
     void Start()
@@ -15,21 +24,109 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) && this.transform.position.y < -1.592938) 
+        playerMovement();
+    }
+
+    void playerMovement()
+    {
+		if(hitting == false && damage == false) 
 		{
-			this.transform.Translate(new Vector3(0,1,1.2f) * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.W) && this.transform.position.y < -1f)
+        {
+            this.transform.Translate(new Vector3(0, 1, 1.2f) * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A) && Cam2d.WorldToScreenPoint(this.transform.position).x > 20)
+        {
+            this.transform.Translate(Vector3.left * Time.deltaTime * speed);
+            //SPRITE ROTATION LEFT
+            playerIdle.transform.rotation = new Quaternion(0, 0, 0, 0);
+            playerMove.transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S) && this.transform.position.y > -3.0f)
+        {
+            this.transform.Translate(new Vector3(0, -1, -1.2f) * Time.deltaTime * speed);
+
+        }
+        if (Input.GetKey(KeyCode.D) && Cam2d.WorldToScreenPoint(this.transform.position).x < 1440)
+        {
+            this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+            //SPRITE ROTATION RIGHT
+            playerIdle.transform.rotation = new Quaternion(0, 180, 0, 0);
+            playerMove.transform.rotation = new Quaternion(0, 180, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 3;
+        }
+        else
+        {
+            speed = 2;
+        }
+            
+        //ANIMATION
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            playerIdle.SetActive(false);
+            playerMove.SetActive(true);
+        }
+        else
+        {
+            playerIdle.SetActive(true);
+            playerMove.SetActive(false);
+        }
 		}
-		if(Input.GetKey(KeyCode.A)) 
+		else if(damage == false)
 		{
-			this.transform.Translate(Vector3.left * Time.deltaTime * speed);
+			if(Input.GetKey(KeyCode.Mouse0)) 
+			{
+				playerHitNormal.transform.rotation = playerMove.transform.rotation;
+				playerIdle.SetActive(false);
+				playerMove.SetActive(false);
+				playerHitNormal.SetActive(true);
+			}
 		}
-		if(Input.GetKey(KeyCode.S) && this.transform.position.y > -2.86f) 
+        else
+        {
+            timerdamage -= Time.deltaTime;
+            if(timerdamage <= 0)
+            {
+                life -= 10;
+                playerReceivesDamage.SetActive(false);
+                timerdamage = 1;
+                damage = false;
+            }
+            else
+            {
+                playerIdle.SetActive(false);
+                playerMove.SetActive(false);
+                playerHitNormal.SetActive(false);
+                playerReceivesDamage.SetActive(true);
+            }
+        }
+        if(life <= 0)
+        {
+            //playerDeath
+        }
+        //HIT 
+		if(Input.GetKey(KeyCode.Mouse0)) 
 		{
-			this.transform.Translate(new Vector3(0,-1,-1.2f) * Time.deltaTime * speed);
+			hitting = true;
 		}
-		if(Input.GetKey(KeyCode.D)) 
+		else 
 		{
-			this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+			hitting = false;
+			playerHitNormal.SetActive(false);
+		}
+        //RUN
+		if(Input.GetKey(KeyCode.LeftShift)) 
+		{
+			speed = 4;
+		}
+		else 
+		{
+			speed = 2;
 		}
     }
+
 }
