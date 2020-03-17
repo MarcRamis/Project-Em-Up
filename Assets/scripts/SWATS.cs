d2f3edaea1c06cd9a2878a188a1739ad35af5e20
Marc Ramis <marc.ramis@enti.cat>
2020-03-17 16:31:03 +0100

    Swat sprites

--- Assets/scripts/SWATS.cs
+++ Assets/scripts/SWATS.cs
@@ -8,14 +8,17 @@ public class SWATS : MonoBehaviour
     public Camera Cam2d;
     public float speed;
 
+
     // Update is called once per frame
-    /*
+    
     void Update()
     {
+        /*
         if (Vector3.Distance(this.transform.position, player) < 3)
         {
             this.transform.position -= transform.position.normalized * Time.deltaTime * speed;
         }
+        */
     }
-    */
+    
 }
