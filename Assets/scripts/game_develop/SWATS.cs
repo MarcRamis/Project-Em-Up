7020de08aaa2a7a449e4abe715e68eeb01c62e07
Marc Ramis <marc.ramis@enti.cat>
2020-03-19 22:20:44 +0100

    Inici SWAT

--- Assets/scripts/game_develop/SWATS.cs
+++ Assets/scripts/game_develop/SWATS.cs
@@ -7,18 +7,35 @@ public class SWATS : MonoBehaviour
 	public GameObject player;
     public Camera Cam2d;
     public float speed;
-
+    public int health;
+    public float timerMove;
+    Vector3 move;
 
     // Update is called once per frame
     
     void Update()
     {
-        /*
-        if (Vector3.Distance(this.transform.position, player) < 3)
+        Vector3 rotVectorEnemy = Cam2d.WorldToScreenPoint(this.transform.position);
+        Vector3 rotVectorEnemy2 = Cam2d.WorldToScreenPoint(player.transform.position);
+
+        if (health > 0)
         {
-            this.transform.position -= transform.position.normalized * Time.deltaTime * speed;
+            if ( (Cam2d.WorldToScreenPoint(this.transform.position).x > 20 && Cam2d.WorldToScreenPoint(this.transform.position).x < 1300) )
+            {
+                if (rotVectorEnemy.x - rotVectorEnemy2.x > 0)
+                    this.transform.rotation = new Quaternion(0, 0, 0, 0);
+                if (rotVectorEnemy.x - rotVectorEnemy2.x <= 0)
+                    this.transform.rotation = new Quaternion(0, 180, 0, 0);
+
+                if (Vector3.Distance(this.transform.position, player.transform.position) < 3 && timerMove > 3f && Cam2d.WorldToScreenPoint(this.transform.position).z < 2)
+                {
+                    this.transform.position -= ((new Vector3(player.transform.position.x - this.transform.position.x, player.transform.position.y - this.transform.position.y, 
+                        this.transform.position.z)).normalized * Time.deltaTime * speed);
+                }
+            }
+
         }
-        */
+     
     }
     
 }
