b51c5575890bb14396d9473d0bb7bf8b215f65c0
xdman21 <xaviflores1@gmail.com>
2020-04-03 16:34:27 +0200

    Nou fons en el joc

---
+++ Assets/scripts/level1/bg_movement_level1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bg_movement_level1 : MonoBehaviour
+{
+    public GameObject cameraPlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(cameraPlayer.transform.position.x / 100, this.transform.position.y, this.transform.position.z);
+    }
+}
