011ec050da99644f554801d59dc6333a1b30fe70
xavifm <60983549+xavifm@users.noreply.github.com>
2020-04-21 17:39:57 +0200

    Nova escena anomenada com: Level_1_Final feta sobre les pautes del GDD

---
+++ Assets/scripts/UI/popup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popup : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            Time.timeScale = 1;
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Assets/scripts/level1/enemiesBehind.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemiesBehind : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject enemiesbehind;
+
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+        if ((this.transform.position.x - camera.transform.position.x) <= 6)
+        {
+            enemiesbehind.SetActive(true);
+        }
+    }
+}
