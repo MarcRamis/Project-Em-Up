66d9bbb93d3152471210a67bb9d575af622954b5
xavifm <60983549+xavifm@users.noreply.github.com>
2020-04-29 15:57:09 +0200

    Afegit nou sistema de càrrega de vides entre nivells

---
+++ Assets/scripts/game_develop/life_assigner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class life_assigner : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject endLevel;
+    public int lifes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(endLevel != null && endLevel.active == true)
+        {
+            lifes = player.GetComponent<playerController>().vidas;
+        }
+        else if(endLevel == null)
+        {
+            player = GameObject.Find("Player");
+            player.GetComponent<playerController>().vidas = lifes;
+            Destroy(this.gameObject);
+        }
+    }
+}
