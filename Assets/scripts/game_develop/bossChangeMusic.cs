306417abd3adbead3701287522bee0e02dfcd8fd
xavifm <60983549+xavifm@users.noreply.github.com>
2020-05-21 19:55:42 +0200

    Nou retoc afegit

--- Assets/scripts/game_develop/bossChangeMusic.cs
+++ Assets/scripts/game_develop/bossChangeMusic.cs
@@ -6,6 +6,7 @@ public class bossChangeMusic : MonoBehaviour
 {
     public GameObject player;
     public GameObject bossIntro;
+    public GameObject bossMusic;
     public GameObject bossEnable;
     public GameObject music;
     public float bossTimer = 3.2f;
@@ -29,6 +30,7 @@ public class bossChangeMusic : MonoBehaviour
             bossTimer -= Time.deltaTime;
             if(bossTimer > 0)
             {
+                bossMusic.SetActive(true);
                 player.GetComponent<Rigidbody>().velocity = Vector3.zero;
                 player.GetComponent<playerController>().move = false;
                 bossIntro.SetActive(true);
